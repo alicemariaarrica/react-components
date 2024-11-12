da85e372f5e7ecf80904b02346dea4045d36a669
alicemariaarrica <alicemariaarrica@gmail.com>
2024-11-12 15:32:17 +0100

    imgcard.jsx-commit

--- hw-react-components/src/components/imgcard.jsx
+++ hw-react-components/src/components/imgcard.jsx
@@ -1,31 +1,17 @@
-import Button from '..components/Button';
-import img from ../ public / koda.png
 
-export default function createButton() {
+import Button from '../components/Button';
+import img from '../public/koda.png';
 
+export default function CreateImgCard() {
     return (
-
         <div className='imgcard'>
-
-            <img src={img} />
-
-
-
+            <img src={img} alt="descrizione immagine" />
             <div className="description">
-
-                <h3>Titolo del post</h3>
-
-                <p>Descrizione lorem ipsum etc</p>
-
-
-
-
-
-
+                <h3>My favorite Disney movie</h3>
+                <p>Descrizione</p>
                 <Button />
-
-
             </div>
-
-            )
+        </div>
+    );
 }
+
