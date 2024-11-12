b3cc36b4a7429dae6b4fe139208a59ca7547130b
alicemariaarrica <alicemariaarrica@gmail.com>
2024-11-12 15:40:41 +0100

    btn.jsx-and-btn.css-commit

---
+++ hw-react-components/src/components/bottone/btn.jsx
@@ -0,0 +1,12 @@
+import style from "../bottone/btn.css"
+
+export default function button() {
+
+    return (
+
+        <div className="btn-container">
+            <button className={style.btn}>Leggi di più</button>
+
+
+            )
+}
